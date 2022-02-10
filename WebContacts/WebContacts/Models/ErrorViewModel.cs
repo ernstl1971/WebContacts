d82a1cc4cfe7ecb6efd1e1dcf5b6f8f64dc3c1fc
Ernst Jürgen Greif <ernstjuergen.greif@outlook.com>
2022-02-10 08:09:21 +0100

    Initial Checkin

---
+++ WebContacts/WebContacts/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebContacts.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
